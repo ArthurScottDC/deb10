12489e1422b6c27d065264d6c78990d5a906aa3e
Arthur Scott <63733953+ArthurScottDC@users.noreply.github.com>
2020-11-05 02:07:28 -0500

    Update deb10.bash

--- deb10.bash
+++ deb10.bash
@@ -6,6 +6,6 @@ sudo timedatectl set-timezone America/New_York
 sudo apt update
 sudo apt -y install wget
 sudo apt -y full-upgrade
-sudo apt update
-wget -O pihole-install.sh https://install.pi-hole.net
+wget -O pihole-install.sh https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh
 wget https://raw.githubusercontent.com/ArthurScottDC/openvpn-install/master/openvpn-install.sh
+sudo reboot

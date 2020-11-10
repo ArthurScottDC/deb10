7a48515acfddc7ef2018ddc864b1d9432e87e773
Arthur Scott <63733953+ArthurScottDC@users.noreply.github.com>
2020-11-10 00:13:23 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,7 +10,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo reboot
